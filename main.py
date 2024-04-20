310bd92bdef35d3dca69029fdfa66ff60d5cbf9d
Mike <mike.holland@northstar-tek.com>
2024-04-20 18:51:51 -0500

    user signup and login working

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
-from app.model import PostSchema
+from fastapi import FastAPI, Body
 from app.model import PostSchema, UserSchema, UserLoginSchema
 from app.auth.jwt_handler import signJWT
 
@@ -43,6 +42,7 @@ def get_one_post(id: int):
                 "data": post
             }
 
+
 @app.post("/posts", tags=["posts"])
 def add_post(post: PostSchema):
     post.id = len(posts) + 1
@@ -51,3 +51,24 @@ def add_post(post: PostSchema):
         "info": "Post added successfully"
     }
 
+
+@app.post("/user/signup", tags=["user"])
+def user_signup(user: UserSchema = Body(default=None)):
+    users.append(user)
+    return signJWT(user.email)
+
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+        return False
+
+@app.post("/user/login", tags=["user"])
+def user_login(user: UserLoginSchema = Body(default=None)):
+    if check_user(user):
+        return signJWT(user.email)
+    else:
+        return {
+            "error": "Invalid credentials"
+        }
